841e5d2041858f821e5fc2dc52ca5c626b593843
Mike <gorczynski.mike@gmail.com>
2018-09-23 09:27:12 +0200

    added string case and string interpolation exercises from rosetta code

---
+++ src/test/java/com/gorczynskimike/stringinterpolation/StringInterpolationDemonstration.java
@@ -0,0 +1,14 @@
+package com.gorczynskimike.stringinterpolation;
+
+import org.junit.Test;
+
+//Not really a test, just a demonstration
+public class StringInterpolationDemonstration {
+
+    @Test
+    public void demonstrate() {
+        StringInterpolationSolution stringInterpolationSolution = new StringInterpolationSolution();
+        stringInterpolationSolution.showExamples();
+    }
+
+}
