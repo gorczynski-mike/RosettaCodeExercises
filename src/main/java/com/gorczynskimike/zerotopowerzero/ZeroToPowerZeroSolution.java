63b18e17ecabba47caed1e6150c5eb9dfbd6051a
Mike <gorczynski.mike@gmail.com>
2018-09-23 16:28:29 +0200

    rosetta code : zero to power zero

---
+++ src/main/java/com/gorczynskimike/zerotopowerzero/ZeroToPowerZeroSolution.java
@@ -0,0 +1,9 @@
+package com.gorczynskimike.zerotopowerzero;
+
+public class ZeroToPowerZeroSolution {
+
+    public static void main(String[] args) {
+        System.out.println(Math.pow(0,0));
+    }
+
+}
