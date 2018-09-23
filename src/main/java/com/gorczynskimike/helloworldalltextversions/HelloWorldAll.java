e50801fd2547d995b8dff0cfeb116d3c6d656c39
Mike <gorczynski.mike@gmail.com>
2018-09-23 08:26:27 +0200

    added hello world exercises from rosetta code

---
+++ src/main/java/com/gorczynskimike/helloworldalltextversions/HelloWorldAll.java
@@ -0,0 +1,17 @@
+package com.gorczynskimike.helloworldalltextversions;
+
+public class HelloWorldAll {
+
+    public static void main(String[] args) {
+        //standard text
+        System.out.println("Hello world!");
+        //standard text no new line
+        System.out.print("Hello world!");
+
+        System.out.println(System.lineSeparator());
+
+        //standard error
+        System.err.println("Goodby world!");
+    }
+
+}
